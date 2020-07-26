e37dd1a42babc9c20695076c842af2488c82abb0
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-07-26 15:52:34 +0700

    Chapter 3 JS // Finished 🎢🎨

---
+++ Mozilla Developer Network/Introduction/scripts/main.js
@@ -0,0 +1,44 @@
+// var myHeading = document.querySelector("h1");
+// myHeading.innerHTML = "Hello world!";
+
+// var esKrim = "coklat";
+// if (esKrim === "coklat") {
+//   alert("Yay!, Aku suka coklat");
+// } else {
+//   alert("Yahhhhh, gak mao!");
+// }
+
+// document.querySelector("html").onclick = function () {
+//   alert("Stop pegang pegang gw!");
+// };
+
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc == "images/mozilla.png") {
+    myImage.setAttribute("src", "images/mozilla-dev.png");
+  } else {
+    myImage.setAttribute("src", "images/mozilla.png");
+  }
+};
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+
+function setUserName() {
+  var myName = prompt("Isi namamu dong.");
+  localStorage.setItem("name", myName);
+  myHeading.innerHTML = "Kamu hebat, " + myName;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.innerHTML = "Kamu hebat, " + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
