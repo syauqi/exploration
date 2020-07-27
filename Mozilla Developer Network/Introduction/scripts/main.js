7c50dc1c8bc9fdd876a536fb41e6d2023b72e5a1
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-07-27 22:55:17 +0700

    Adding a little changes

--- Mozilla Developer Network/Introduction/scripts/main.js
+++ Mozilla Developer Network/Introduction/scripts/main.js
@@ -1,17 +1,3 @@
-// var myHeading = document.querySelector("h1");
-// myHeading.innerHTML = "Hello world!";
-
-// var esKrim = "coklat";
-// if (esKrim === "coklat") {
-//   alert("Yay!, Aku suka coklat");
-// } else {
-//   alert("Yahhhhh, gak mao!");
-// }
-
-// document.querySelector("html").onclick = function () {
-//   alert("Stop pegang pegang gw!");
-// };
-
 var myImage = document.querySelector("img");
 
 myImage.onclick = function () {
@@ -21,7 +7,7 @@ myImage.onclick = function () {
   } else {
     myImage.setAttribute("src", "images/mozilla.png");
   }
-};
+}; 
 
 var myButton = document.querySelector("button");
 var myHeading = document.querySelector("h1");
---
+++ Mozilla Developer Network/Introduction/scripts/secondary.js
@@ -0,0 +1,13 @@
+var myHeading = document.querySelector("h1");
+myHeading.innerHTML = "Hello world!";
+
+var esKrim = "coklat";
+if (esKrim === "coklat") {
+  alert("Yay!, Aku suka coklat");
+} else {
+  alert("Yahhhhh, gak mao!");
+}
+
+document.querySelector("html").onclick = function () {
+  alert("Stop pegang pegang gw!");
+};
