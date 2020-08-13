695952aa8bf7955019170f4fe8d7e1887701a2b3
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-08-14 02:18:15 +0700

    Dicoding Submission On Progress ✌

---
+++ Dicoding Indonesia/Submission/assets/scripts/main.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
