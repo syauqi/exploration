7b1d76105ac23e5e5e29d9247ed85dba926f4814
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-08-14 11:11:52 +0700

    Submission Done 😍🙄 fucking 6 hours

--- Dicoding Indonesia/Submission/assets/scripts/main.js
+++ Dicoding Indonesia/Submission/assets/scripts/main.js
@@ -1,13 +1,27 @@
-window.onscroll = function() {myFunction()};
+if (window.screen.width < 780) {
+  document.write('<script src="assets/scripts/mobile.js"></script>');
+}
+
+window.onscroll = function () {
+  Lol();
+};
 
 var navbar = document.getElementById("navbar");
-
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function Lol() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
